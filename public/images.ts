a452abe4a1d4b44aeba73515401d24a0bc817640
marshalljunior007 <marshalljunior007@gmail.com>
2025-01-08 05:41:49 +0100

    image fix

--- public/images.ts
+++ public/images.ts
@@ -1,11 +1,14 @@
 import AppleClone from './images/Apple.png';
 import Homme from './images/Homme.png';
 import SpaceX from './images/SpaceX.png';
-import Tg from './Images/Tegadgets.png';
+import Tg from './images/Tegadgets.png';
 
-export default {
+
+const images = {
   AppleClone,
   Homme,
   SpaceX,
   Tg,
 };
+
+export default images;
